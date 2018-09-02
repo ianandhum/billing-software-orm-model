e4073a52d929c0b2dc5de54267e034a583bffa74
Anandhu Manoj <anandhumanoj282@gmail.com>
2018-09-02 20:20:08 +0530

    Made the model `Client` capable of binding directly

--- Provider.Data/Client.cs
+++ Provider.Data/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Provider.Data.Support;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
@@ -9,42 +10,160 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Provider.Data
 {
     [Table(Name = "Client")]
-    public class Client
+    public class Client:NotifyUIBase
     {
         public Client()
         {
             this.Invoices = new HashSet<Invoice>();
         }
 
+        private int _ClientId;
 
         [Column(IsPrimaryKey = true, Name = "ClientId", DbType = "INTEGER")]
         [Key]
-        public int ClientId { get; set; }
 
-        public string Name { get; set; }
+        public int ClientId {
+            get
+            {
+                return _ClientId;
+            }
+            set
+            {
+                _ClientId = value;
+                RaisePropertyChanged("ClientId");
+                
+            }
+        }
 
-        public string Email { get; set; }
+        private string _Name;
 
-        public string Telephone { get; set; }
+        public string Name
+        {
+            get { return _Name; }
+            set {
+                _Name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
 
-        public string BillingAddress { get; set; }
+        private string _Email;
 
-        public string BillingZIP { get; set; }
+        public string Email
+        {
+            get { return _Email; }
+            set {
+                _Email = value;
+                RaisePropertyChanged("Email");
+            }
+        }
 
-        public string BillingCity { get; set; }
+        private string _Telephone;
 
-        public string ShippingAddress { get; set; }
+        public string Telephone
+        {
+            get { return _Telephone; }
+            set {
+                _Telephone = value;
+                RaisePropertyChanged("Telephone");
+            }
+        }
 
-        public string ShippingZIP { get; set; }
+        private string _BillingAddress;
 
-        public string ShippingCity { get; set; }
+        public string BillingAddress
+        {
+            get { return _BillingAddress; }
+            set {
+                _BillingAddress = value;
+                RaisePropertyChanged("BillingAddress");
+            }
+        }
 
-        public string Details { get; set; }
+        private string _BillingZIP;
 
-        public string GSTIN { get; set; }
+        public string BillingZIP
+        {
+            get { return _BillingZIP; }
+            set {
+                _BillingZIP = value;
+                RaisePropertyChanged("BillingZIP");
+            }
+        }
+
+        private string _BillingCity;
+
+        public string BillingCity
+        {
+            get { return _BillingCity; }
+            set {
+                _BillingCity = value;
+                RaisePropertyChanged("BillingCity");
+            }
+        }
+
+
+        private string _ShippingAddress;
+
+        public string ShippingAddress
+        {
+            get { return _ShippingAddress; }
+            set
+            {
+                _ShippingAddress = value;
+                RaisePropertyChanged("ShippingAddress");
+            }
+        }
+
+        private string _ShippingZIP;
+
+        public string ShippingZIP
+        {
+            get { return _ShippingZIP; }
+            set
+            {
+                _ShippingZIP = value;
+                RaisePropertyChanged("ShippingZIP");
+            }
+        }
+
+        private string _ShippingCity;
+
+        public string ShippingCity
+        {
+            get { return _ShippingCity; }
+            set
+            {
+                _ShippingCity = value;
+                RaisePropertyChanged("ShippingCity");
+            }
+        }
+
+        private string _Details;
+
+        public string Details
+        {
+            get { return _Details; }
+            set {
+                _Details = value;
+                RaisePropertyChanged("Details");
+            }
+        }
+
+        private string _GSTIN;
+
+        public string GSTIN
+        {
+            get { return _GSTIN; }
+            set {
+                _GSTIN = value;
+                RaisePropertyChanged("GSTIN");
+            }
+        }
+
 
         public virtual ICollection<Invoice> Invoices { get; set; }
 
---
+++ Provider.Data/Support/NotifyUIBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.Data.Support
+{
+    public class NotifyUIBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged( String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
